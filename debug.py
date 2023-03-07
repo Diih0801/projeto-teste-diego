e2efba4c2d0ec22b319d8ad298cb7f6614f219e2
Diego Gomes <diegogoliveira08@gmail.com>
2023-03-06 23:27:15 -0300

    versão 2

---
+++ debug.py
@@ -0,0 +1,14 @@
+print('Olá')
+
+
+def calcular_preco_combo(pizza, refrigerante):
+    total = pizza + refrigerante
+    print(total)
+
+
+calcular_preco_combo(30, 20)
+print('Programa Finalizado')
+# F9 - Marcar um breakpoint.
+# F5 - Abrir modo Debug. 
+# F10 - Passar sobre a linha, SEM entrar em método internos.
+# F11 - Passar sobre a linha e entrar em método interno. 
